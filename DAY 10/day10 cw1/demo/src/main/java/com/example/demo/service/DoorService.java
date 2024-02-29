64d12db9768dede3b38d77b498ed1ff12c044491
lakshana-lakshu <124855819+lakshana-lakshu@users.noreply.github.com>
2024-02-29 09:24:02 +0530

    Add files via upload

---
+++ DAY 10/day10 cw1/demo/src/main/java/com/example/demo/service/DoorService.java
@@ -0,0 +1,57 @@
+package com.example.demo.service;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Door;
+import com.example.demo.repository.DoorRepository;
+
+@Service
+public class DoorService {
+    public DoorRepository doorRepo;
+    public DoorService(DoorRepository doorRepo)
+    {
+        this.doorRepo = doorRepo;
+    }
+    public boolean saveDoor(Door door)
+    {
+        try{
+            doorRepo.save(door);
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+        return true;
+    }
+    public List<Door> getByColor(String color)
+    {
+        return doorRepo.findByColor(color);
+    }
+    public Door updateColor(String color,int id)
+    {
+        doorRepo.updateColorById(color, id);
+        return doorRepo.findById(id).orElse(null);
+    }
+    public boolean deleteDoor(int id)
+    {
+        if(this.getDoorById(id)==null)
+        {
+            return false;
+        }
+        doorRepo.deleteDoorByDoorId(id);
+        return true;
+    }
+    public List<Door> getByDoorType(String doorType)
+    {
+        return doorRepo.findByDoorType(doorType);
+    }
+    public List<Door> getDoors()
+    {
+        return doorRepo.findAll();
+    }
+    public Door getDoorById(int id)
+    {
+        return doorRepo.findById(id).orElse(null);
+    }
+}
